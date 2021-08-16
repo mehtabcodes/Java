30ed1481127f62f3aa687efcd1bee738de273020
haspdecrypted <shubham_sen@srmuniv.edu.in>
2021-08-17 01:04:56 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter you name");
+        String name=sc.next();
+
+        System.out.println("Enter you phone");
+        String numb=sc.next();
+
+        System.out.println("Enter your salary");
+        Float sal=sc.nextFloat();
+
+        System.out.println("Enter your candidate id");
+        int id=sc.nextInt();
+
+        System.out.println(id+ " " + name + " " + numb + " " + sal );
+    }
+    
+}
