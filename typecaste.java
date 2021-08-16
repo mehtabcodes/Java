379f11046f31a6254653075fce9bdb8cf076accb
haspdecrypted <shubham_sen@srmuniv.edu.in>
2021-08-17 01:04:58 +0530

    Create typecaste.java

---
+++ typecaste.java
@@ -0,0 +1,11 @@
+public class typecaste {
+    public static void main(String[]args) {
+     
+        //here we will type caste a float type into an int tyoe value
+        int num = (int)(82.55f);
+        // in the output the character part is only taken not the mantissa part
+        System.out.println(num);
+
+    }
+    
+}
