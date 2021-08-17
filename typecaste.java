d7a48df6ce06d58618f4f796818f49d2268b5d82
haspdecrypted <shubham_sen@srmuniv.edu.in>
2021-08-18 01:24:42 +0530

    Update typecaste.java

--- typecaste.java
+++ typecaste.java
@@ -1,10 +1,15 @@
 public class typecaste {
     public static void main(String[]args) {
      
-        //here we will type caste a float type into an int tyoe value
+        //here we will type caste a float type into an int type value
         int num = (int)(82.55f);
         // in the output the character part is only taken not the mantissa part
         System.out.println(num);
+
+        //here we will type caste a double type into an float type value
+        float num1 = (float)(85.695578451216589d);
+        // double chnaged into float
+        System.out.println(num1);
 
     }
     
