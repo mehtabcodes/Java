cca9c6d9b764805fe76fa4de8360ac6b6a17b784
haspdecrypted <shubham_sen@srmuniv.edu.in>
2021-08-18 19:50:36 +0530

    Create largestoftwo.java

---
+++ largestoftwo.java
@@ -0,0 +1,11 @@
+public class largestoftwo {
+    public static void main(String[]args){
+        int a=44,b=6;
+        if(a>b){
+            System.out.println("a is largest");
+        }
+        else
+        System.out.println("b is largest");
+    }
+    
+}
