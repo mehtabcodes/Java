2f4c22c7f3958d5da5ff554522982a3a9e1d37ed
haspdecrypted <shubham_sen@srmuniv.edu.in>
2021-08-18 19:50:39 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,38 @@
+public class loops {
+    public static void main(String[]args){
+
+        //open the comments and run the part you wish to test
+    /*    //if condition
+        int dob = 4 ;
+        if(dob==4)
+        System.out.println("It's correct !");
+    */
+
+    /*
+        //while condition
+        int count=0;
+        while(count!=8){
+        System.out.println(count);
+        count++;
+        }
+        //System.exit(0);
+    */   
+    /*
+        //for loop
+        for(int i =1; i <=10; i++)
+        {
+            System.out.println(i);
+        }
+    */
+
+    /*
+        //if-else
+        int age=17;
+        if(age >=18){
+            System.out.println("You can vote!");
+        }
+        else
+        System.out.println(age+" is not eligible to vote the minimum age required is 18");
+    */
+    }
+}
