d58510b36857e28b81ae02183f27750b88f86845
haspdecrypted <shubham_sen@srmuniv.edu.in>
2021-08-18 19:50:25 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,14 @@
+public class evenodd {
+    public static void main(String[]args){
+        int num=1903;
+
+        //condition for even
+        if(num%2==0){
+            System.out.println(num+" is even");
+        }
+        //condition for odd
+        else
+        System.out.println(num+" is odd");
+    }
+    
+}
